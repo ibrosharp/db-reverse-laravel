6027b2c6062c749a3cf16a10c2582663560942d7
ibro <street@localhost.localdomain>
2020-09-24 20:45:52 +0000

    Implement creator file

--- src/Creators/CrudControllerCreator.php
+++ src/Creators/CrudControllerCreator.php
@@ -16,6 +16,7 @@ class CrudControllerCreator implements FileCreator {
     private $fileName;
     private $path;
     private $model;
+    private $modelVariable;
     public function __construct() 
     {
         $this->path = ConfigState::getFileSystemConfiguration()["output_dir"]."/controllers";
@@ -41,6 +42,8 @@ class CrudControllerCreator implements FileCreator {
 
         $this->fileName = $this->className.".php";
 
+        $this->modelVariable = strtolower($this->model);
+
     }
 
     public function createFile() : void {   
@@ -49,12 +52,16 @@ class CrudControllerCreator implements FileCreator {
 
         Interactor::sendMessage("Creating : {$this->fileName}");
 
+        $modelSeek = $this->modelSeek();
+        $validation = $this->validations();
+        $tableFiller = $this->tableFillerArray();
+
         $this->writeToFile($this->wrapFrame(
             $this->generateGetAllFunction(),
             $this->generateSingleFunction(),
-            $this->generateCreateFunction(),
-            $this->generateUpdateFunction(),
-            $this->generateDeleteFunction()
+            $this->generateCreateFunction($modelSeek,$validation,$tableFiller),
+            $this->generateUpdateFunction($modelSeek,$validation,$tableFiller),
+            $this->generateDeleteFunction($modelSeek)
         ));
 
         Interactor::sendSucceessMessage("Created :{$this->fileName}");
@@ -62,28 +69,191 @@ class CrudControllerCreator implements FileCreator {
     }
 
     private function generateGetAllFunction() : string {
-        return "";
+        $content = "".
+        "\tpublic function index() : JsonResponse\n".
+        "\t{\n\n".
+
+            "\t\t\${$this->modelVariable} = {$this->model}::paginate(20);\n\n".
+    
+            "\t\treturn response()->json(['data' => \${$this->modelVariable}, 'status'=> true,'message' => '{$this->modelVariable} fetch successfully'],200);\n\n".
+
+        
+        "\t}\n\n";
+
+        return $content;
     }
 
     private function generateSingleFunction() : string {
         return "";
     }
 
-    private function generateCreateFunction() : string {
-        return "";
+    private function generateCreateFunction(string $modelSeek,string $validation, string $tableFiller) : string {
+
+        $content = "".
+        "\tpublic function create(Request \$request) : JsonResponse\n".
+        "\t{\n\n".
+
+            $validation.
+
+            $modelSeek.
+
+            "\t\ttry{\n\n".
+
+                "\t\t\t\${$this->modelVariable} = {$this->model}::create(\n".
+                    $tableFiller.
+                "\t\t\t);\n\n".
+
+            "\t\t}catch(Exception \$e) {\n\n". 
+
+                "\t\t\tLog::info(\$e)\n". 
+                "\t\t\treturn response()->json(['status'=> false,'message' => 'failed to update {$this->modelVariable}'],500);\n\n".
+
+            "\t\t}\n\n".
+
+            "\t\treturn response()->json(['data' => \${$this->modelVariable}, 'status'=> true,'message' => '{$this->modelVariable} created successfully'],200);\n\n".
+
+        "\t}\n\n";
+
+        return $content;
+        
     }   
 
-    private function generateUpdateFunction() : string {
-        return "";
+    private function generateUpdateFunction(string $modelSeek,string $validation,string $tableFiller) : string {
+
+        $content = "".
+        "\tpublic function update(Request \$request,\$id)\n".
+        "\t{\n\n".
+
+            $validation.
+    
+            $modelSeek.
+    
+            "\t\ttry{\n\n".
+
+                "\t\t\t\${$this->modelVariable}->update(\n".
+                    $tableFiller.
+                "\t\t\t);\n\n".
+
+            "\t\t}catch(Exception \$e) {\n\n". 
+
+                "\t\t\tLog::info(\$e)\n". 
+                "\t\t\treturn response()->json(['status'=> false,'message' => 'failed to update {$this->modelVariable}'],500);\n\n".
+
+            "\t\t}\n\n".
+    
+            "\t\treturn response()->json(['data' => \${$this->modelVariable}, 'status'=> true,'message' => '{$this->modelVariable} updated successfully'],200);\n\n".
+
+        "\t}\n\n";
+
+        return $content;
     }
 
-    private function generateDeleteFunction() : string {
-        return "";
+    private function generateDeleteFunction(string $modelSeek) : string {
+       
+        $content ="".
+        "\tpublic function destroy(\$id) : JsonResponse\n".
+        "\t{\n\n".
+
+            $modelSeek.
+          
+
+            "\t\ttry{\n\n".
+    
+                "\t\t\t\${$this->modelVariable}->delete();\n\n".
+
+            "\t\t}catch(Exception \$e) {\n\n". 
+
+                "\t\t\tLog::info(\$e)\n". 
+                "\t\t\treturn response()->json(['status'=> false,'message' => 'failed to delete {$this->modelVariable}'],500);\n\n".
+
+            "\t\t}\n\n".
+    
+            "\t\treturn response()->json(['data' => \${$this->modelVariable}, 'status'=> true,'message' => '{$this->modelVariable} deleted successfully'],200);\n\n".
+        
+        "\t}\n\n";
+
+        return $content;
+    }
+
+    private function  validations() : string  {
+        $content = "";
+        //"\t\t\$validator = Validator::make(\$request->all(),[\n";
+        //         "image" => "",
+        //         "first_name" => "required|string",
+        //         "phone_number" => "required|numeric|digits:11",
+        //         "last_name" => "required|string"
+        //     ]);
+
+           //     if($validator->fails()) return $this->failureResponse(401,$validator->errors()->first());\
+
+        return $content;
+    }
+
+    private function modelSeek() :  string {
+
+        $content = "".
+
+        "\t\t\${$this->modelVariable} = {$this->model}::find(\$id);\n\n".
+    
+        "\t\tif(!\${$this->modelVariable}) return response()->json(['status'=> false,'message' => '{$this->modelVariable} not found'],404);\n\n";
+
+        return $content;
+    }
+
+    private function tableFillerArray() : string {
+
+        $content = "\t\t\t\t[\n";
+
+        foreach($this->table->getColums() as $column) {
+            if($column->isPrimary() || $column->isTimestamp()) continue;
+            $content .= "\t\t\t\t\t'{$column->getName()}' => \$request->{$column->getName()},\n";
+        }
+
+        $content .= "\t\t\t\t]\n";
+
+        return $content;
     }
 
 
     private function wrapFrame(string $all,string $single,string $create,string $update,string $delete) : string {
-        return "";
+        $content = "".
+        
+        "<?php\n\n".
+
+        "namespace App\Http\Controllers;\n\n".
+
+        "use App\Http\Controllers\Controller;\n".
+        "use App\{$this->model};\n".
+        "use Exception;\n".
+        "use Illuminate\Http\JsonResponse;\n".
+        "use Illuminate\Http\Request;\n".
+        "use Illuminate\Support\Facades\Log;\n".
+        "use Illuminate\Support\Facades\Validator;\n\n".
+
+        "class {$this->className} extends Controller\n".
+        "{\n\n".
+
+            "\t/**\n".
+            "\t* Create a new controller instance.\n".
+            "\t* \n".
+            "\t* @return void\n".
+            "\t*/\n".
+            "\tpublic function __construct()\n".
+            "\t{\n\n".
+
+            "\t}\n\n". 
+
+            $all.
+            $single.
+            $create. 
+            $update. 
+            $delete.
+
+        "}";
+   
+
+        return $content;
+   
        
     }
 
--- src/DataObject/Column.php
+++ src/DataObject/Column.php
@@ -31,6 +31,17 @@ class Column {
         return $this->name;
     }
 
+    public function isPrimary() {
+        if(strtolower($this->key) == "pri" ) return true;
+        return false;
+    }
+
+    public function isTimestamp() {
+        if(($this->name == "created_at" || $this->name == "updated_at") && strtolower($this->nullable) == "yes" ) return true;
+
+        return false;
+    }
+
     public function toSchemaString() : string {
 
         $eloquentDataType = EloquentTranslator::translateDataType($this->dataType,$this->name);
